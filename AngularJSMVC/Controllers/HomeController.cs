d26508c69bdad576a30d057ae6633f820076072b
Ezhil <itzezhil@gmail.com>
2018-09-16 00:29:16 -0400

    base

---
+++ AngularJSMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ngbind()
+        {
+            return View();
+        }
+
+        public ActionResult nginit()
+        {
+            return View();
+        }
+
+
+        public ActionResult ngexpression()
+        {
+            return View();
+        }
+
+        public ActionResult ngcontroller()
+        {
+            return View();
+        }
+
+        public ActionResult routing()
+        {
+            return View();
+        }
+
+        public ActionResult filters()
+        {
+            return View();
+        }
+    }
+
+}
